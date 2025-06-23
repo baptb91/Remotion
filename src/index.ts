590fe243713d88c1ace3e9ce616cdeca35c5cb98
Baptiste <148646193+baptb91@users.noreply.github.com>
2025-06-23 18:39:48 +0200

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,15 @@
-import { registerRoot } from "remotion";
-import { RemotionRoot } from "./Root";
+import { Composition } from "remotion";
+import { MyComponent } from "./MyComponent";
 
-registerRoot(RemotionRoot);
+export const RemotionRoot: React.FC = () => (
+  <>
+    <Composition
+      id="MyComp"
+      component={MyComponent}
+      durationInFrames={300}
+      fps={30}
+      width={1920}
+      height={1080}
+    />
+  </>
+);
