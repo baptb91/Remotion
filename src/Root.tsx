21cea6f979615096a0b3d774df9c38064addc69d
Baptiste <148646193+baptb91@users.noreply.github.com>
2025-06-24 14:31:58 +0200

    Update Root.tsx

--- src/Root.tsx
+++ src/Root.tsx
@@ -1,17 +1,15 @@
 import { Composition } from "remotion";
-import { MyComposition } from "./Composition";
+import { MyComponent } from "./Component";
 
-export const RemotionRoot: React.FC = () => {
-  return (
-    <>
-      <Composition
-        id="MyComp"
-        component={MyComposition}
-        durationInFrames={60}
-        fps={30}
-        width={1280}
-        height={720}
-      />
-    </>
-  );
-};
+export const RemotionRoot: React.FC = () => (
+  <>
+    <Composition
+      id="MyComp"
+      component={MyComponent}
+      durationInFrames={300}
+      fps={30}
+      width={1920}
+      height={1080}
+    />
+  </>
+);
